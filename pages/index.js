d8092f162435c6f586ea8f0047da2482264ae180
Upendra Kumar <upendrakumarvipin@gmail.com>
2022-02-21 21:50:24 +0530

    css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Script from 'next/script'
+import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
@@ -17,10 +17,10 @@ export default function Home() {
 
       <nav className={styles.mainnav}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Blog</li>
-          <li>Contact</li>
+          <Link href={'/'}><a><li>Home</li></a></Link>
+          <Link href={'/about'}><a><li>About</li></a></Link>
+          <Link href={'/blog'}><a><li>Blog</li></a></Link>
+          <Link href={'/contact'}><a><li>Contact</li></a></Link>
         </ul>
       </nav>
       <main className={styles.main}>
